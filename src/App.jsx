7bc9937b7252ebf466bea8d1fbfa69860e22b043
Ch_wassim <chelghoum.mohammedouassim@gmail.com>
2024-09-20 22:00:05 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/navBar"
+import Hero from "./components/hero"
+import Highlight from "./components/Highlight"
+import Model from "./components/Model"
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      
+      <NavBar/>
+      <Hero/>
+      <Highlight/>
+      <Model/>
+
+    </main>
+  )
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div>
+      <Html>
+
+        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
+
+            <div className='w-[10vw] h-[10vw] rounded-full'>
+                Loading ...
+            </div>
+
+        </div>
+
+      </Html>
+    </div>
+  )
+}
+
+export default Loader
